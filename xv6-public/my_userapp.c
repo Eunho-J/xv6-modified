c84b5c936a7a669f175c306b32cf559c3c5cf075
정은호 <2017030246@hanyang.ac.kr>
2021-03-17 10:41:07 +0900

    lab02_fin_seperateUserProcess_test.c

--- xv6-public/my_userapp.c
+++ xv6-public/my_userapp.c
@@ -7,12 +7,7 @@ main(int argc, char *argv[])
 {
 	char *buf = "Hello xv6!";
 	int ret_val;
-	int pid, ppid;
 	ret_val = myfunction(buf);
 	printf(1, "Return value : 0x%x\n", ret_val);
-	pid = getpid();
-	ppid = getppid();
-	printf(1, "My pid is %d\nMy ppid is %d\n", pid, ppid);
-
 	exit();
 }
