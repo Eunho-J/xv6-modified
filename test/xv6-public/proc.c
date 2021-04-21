f3605459098fa5776003d3e7a36d56ecda95e9cc
Eunho.J <jho6394@hanyang.ac.kr>
2021-04-22 00:30:12 +0900

    test

--- test/xv6-public/proc.c
+++ test/xv6-public/proc.c
@@ -318,6 +318,10 @@ exit(void)
 
   // Jump into the scheduler, never to return.
   curproc->state = ZOMBIE;
+  curproc->p_node.level = 0;
+  curproc->p_node.turnCount = 0;
+  curproc->p_node.distance = 0;
+  curproc->p_node.share = 0;
   sched();
   panic("zombie exit");
 }
